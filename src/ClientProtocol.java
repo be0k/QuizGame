5bef3c90906218e0353da168b469392b04f3ad50
be0k <befinetuning@gmail.com>
2024-11-12 12:00:54 +0900

    add protocol based coding

---
+++ src/ClientProtocol.java
@@ -0,0 +1,162 @@
+import java.io.*;
+import java.net.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class ClientProtocol {
+	static JButton button;
+	static String text1;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BufferedReader in = null;
+		BufferedWriter out = null;
+		Socket socket = null;
+		int q_num = 0;
+		int idx = 0;
+		
+		// JFrame generation (GUI)
+        JFrame frame = new JFrame("Quiz Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);  // 창 크기 설정
+
+        // Layout setup
+        frame.setLayout(new FlowLayout());
+
+        // components generation
+        JLabel label1 = new JLabel();
+        JTextField textField = new JTextField(15);
+        button = new JButton("Submit");
+        JLabel label2 = new JLabel("결과");
+        JLabel label3 = new JLabel();
+        
+        //button action
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                button.setEnabled(false);
+            }
+        });
+        
+        frame.add(label1);
+        frame.add(textField);
+        frame.add(button);
+        frame.add(label2);
+        frame.add(label3);
+
+        // visualize on screen
+        frame.setVisible(true);
+        
+        
+        //Connect to socket
+        try {
+			socket = new Socket ("localhost", 9999);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        } catch (IOException e) {
+        	System.out.println("소켓 연결 중 오류가 발생했습니다.");
+        }
+        
+        
+		//receive the number of questions
+		try {
+			out.write("qgp/0/0" + '\n');
+			out.flush();
+			q_num = Integer.parseInt(in.readLine());
+		}catch (IOException e) {
+			System.out.println("질문의 개수를 받아오는 동안 오류가 발생했습니다.");
+		}
+		
+		
+		//repeat
+		idx = 0;
+		while(true) {
+			
+			//complete all quiz
+			if(idx == q_num) {
+				//receive result score
+				try {
+					out.write("qgp/3/0" + '\n');
+					out.flush();
+					
+					q_num = Integer.parseInt(in.readLine());
+				}catch (IOException e) {
+					System.out.println("서버에서 점수를 불러오는 중에 오류가 발생했습니다..");
+				}
+				
+				label3.setText("\n\n\n\n\n\n\n\n\nFinal Score : " + q_num);
+				System.out.println("Final Score : " + q_num);
+				break;
+			}
+			
+			//This protocol make client to solve "one" question.
+			try {
+			out.write("qgp/1/" + idx + '\n');
+			out.flush();
+			
+			} catch (IOException e) {
+				System.out.println("서버와 통신 중에 오류가 발생했습니다.");
+			}
+			
+			//receive question
+			try {
+				String inputMessage = in.readLine();
+				label1.setText(inputMessage);
+				System.out.println("Qusetion: " + inputMessage);
+				
+			} catch (IOException e) {
+				System.out.println("서버에서 문제를 받아오는 중에 오류가 발생했습니다.");
+			}
+			
+			//wait button click
+			button.setEnabled(true);
+			waitForAnswer();
+
+			//write answer
+			try {
+				String outputMessage = textField.getText();
+				out.write("qgp/2/" + idx + '/' + outputMessage + '\n');
+				out.flush();
+
+				
+			} catch (IOException e) {
+				System.out.println("서버에 답을 제출하는 중에 오류가 발생했습니다.");
+			}
+			
+			//receive result
+			try {
+				String resultMessage = in.readLine();
+				label2.setText("<html>결과<br>" + resultMessage + "</html>");
+				System.out.println("Result: " + resultMessage);
+			}catch (IOException e) {
+				System.out.println("서버에서 결과를 불러오는 중에 오류가 발생했습니다.");
+			}
+			idx++;
+		}
+
+		try {
+			if (socket != null) {
+				System.out.println("close");
+				socket.close();
+			}
+			
+		}catch (IOException e) {
+			System.out.println("소켓을 닫는 중에 오류가 발생했습니다.");
+		}
+	}
+	
+	
+	private static void waitForAnswer() {
+        while (button.isEnabled()) {
+            try {
+                Thread.sleep(100); // 버튼이 활성화될 때까지 대기
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
---
+++ src/ServerProtocol.java
@@ -0,0 +1,139 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class ServerProtocol implements Runnable {
+	
+	private Socket clientSocket;
+
+    public ServerProtocol(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+    
+	//question Method
+	public static String[][] getQnA() {
+        String[][] qna = new String[4][2];
+
+        qna[0][0] = "LLM은 뭐의 약자일까요?\n";
+        qna[0][1] = "LargeLanguageModel";
+
+        qna[1][0] = "JVM은 무엇을 의미할까요?\n";
+        qna[1][1] = "JavaVirtualMachine";
+
+        qna[2][0] = "API는 뭐의 약자일까요?\n";
+        qna[2][1] = "ApplicationProgrammingInterface";
+
+        qna[3][0] = "SQL은 뭐의 약자일까요?\n";
+        qna[3][1] = "StructuredQueryLanguage";
+
+        return qna;
+    }
+	
+	@Override
+    public void run() {
+		// TODO Auto-generated method stub
+		BufferedReader in = null;
+		BufferedWriter out = null;
+//		ServerSocket listener = null;
+//		Socket socket = null;
+
+//			listener = new ServerSocket(9999);
+//			System.out.println("Waiting to connect.....");
+//			socket = listener.accept();
+//			
+//			
+			String[][] qnaArray = getQnA();
+			int cnt = 4;
+			int corr = 0;
+			int flag = 0;
+			System.out.println("Client is connected.");
+			
+			try {
+				in = new BufferedReader(
+						new InputStreamReader(clientSocket.getInputStream()));
+				out = new BufferedWriter(
+						new OutputStreamWriter(clientSocket.getOutputStream()));
+			} catch (IOException e) {
+				System.out.println("소켓 연결 중에 오류가 발생했습니다.");
+			}
+			
+			flag = 0;
+			try {
+				while(true) {
+						String[] inputMessage = in.readLine().split("/");
+						if(inputMessage[0].equalsIgnoreCase("qgp")) {
+							switch(inputMessage[1]) {
+							case "0":
+								out.write(cnt + "\n");
+								out.flush();
+								break;
+							case "1":
+								out.write(qnaArray[Integer.parseInt(inputMessage[2])][0]);
+								out.flush();
+								break;
+							case "2":
+								if(inputMessage[3].replaceAll("\\s", "").equalsIgnoreCase(qnaArray[Integer.parseInt(inputMessage[2])][1])) {
+									
+									//correct case
+									out.write("correct\n");
+									out.flush();
+									corr++;
+								}else {
+									
+									//incorrect case
+									out.write("incorrect\n");
+									out.flush();
+								}
+								
+								break;
+							case "3":
+								out.write(corr + "\n");
+								out.flush();
+								flag = 1;
+								break;
+							default:
+								System.out.println("protocol 해석 중에 오류가 발생했습니다.");
+								break;
+							}
+							if (flag==1) {
+
+								break;
+							}
+							
+						}else {
+							System.out.println("정상적인 프로토콜이 아닙니다.");
+							break;
+						}
+				}
+			}catch (IOException e) {
+				System.out.println("protocol을 불러오는 중 오류가 발생했습니다.");
+			}finally {
+				try {
+					if(clientSocket != null)
+						System.out.println("close");
+						clientSocket.close();
+				} catch (IOException e) {
+					System.out.println("Error was occured During chatting with client.");
+				}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		//open the server socket and waiting for connecting client
+        ServerSocket serverSocket = new ServerSocket(9999);
+        System.out.println("Server is listening on port 9999...");
+
+        //execute client connection using ExecutorService
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        // receive client connection through while
+        while (true) {
+            Socket clientSocket = serverSocket.accept();  // waiting for client connection
+            System.out.println("New client connected");
+
+            //Submit to threadPool after generate Server object that excute client connection
+            executorService.submit(new ServerProtocol(clientSocket));
+        }
+    }
+}
